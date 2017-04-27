220a11a4a48ef87aacedcd76daa87259d6aaea00
Caishun Chen <cschen1205@gmail.com>
2017-04-27 16:41:23 +0800

    add in ANN, NBC, and decision tree implementation for lua

---
+++ src/NaiveBayseClassifier.lua
@@ -0,0 +1,49 @@
+local NaiveBayseClassifier={};
+NaiveBayseClassifier.__index=NaiveBayseClassifier;
+
+function NaiveBayseClassifier.create()
+	local nbc={};
+	setmetatable(nbc, NaiveBayseClassifier);
+	
+	return nbc;
+end
+
+function NaiveBayseClassifier:predict(record, records, classAttribute)
+	local predictedValue=nil;
+	local predictedProb=0;
+	local classLabel=classAttribute:getName();
+	
+	for i=1, classAttribute:getValueCount() do
+		local classValue=classAttribute:getValue(i);
+		
+		local CCount=0;
+		for j=1, (# records) do
+			if records[j]:getAttributeValue(classLabel)==classValue then
+				CCount=CCount + 1;
+			end
+		end
+		local classValueProb=CCount / (# records);
+		
+		for attrName, attrValue in next, record.attributes, nil do
+			if attrName ~= classLabel then
+				local fiCount=0;
+				for j=1, (# records) do
+					if records[j]:getAttributeValue(attrName) == attrValue and records[j]:getAttributeValue(classLabel)==classValue then
+						fiCount=fiCount+1;
+					end
+				end
+				local fiProb=fiCount / CCount;
+				classValueProb=classValueProb * fiProb;
+			end
+		end
+		
+		if classValueProb > predictedProb then
+			predictedProb = classValueProb;
+			predictedValue=classValue;
+		end
+	end
+	return predictedValue;
+end
+
+return NaiveBayseClassifier;
+
